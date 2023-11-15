74c4bf18ab6e482a427b25c330a5c9b314e5ba1c
Nate96 <braukhoff96@gmail.com>
2023-11-15 13:12:32 -0600

    Added current directory search and pv

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,23 @@
 local builtin = require('telescope.builtin')
+local utils = require('telescope.utils')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-
 vim.keymap.set('n', '<leader>fgit', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fs', function() 
-   builtin.grep_string({ search = vim.fn.input("Grep > ")})
+
+vim.keymap.set('n', '<leader>fs', function()
+   local input_string = vim.fn.input("Search For > ")
+      if (input_string == '') then
+         return
+      end
+      require("telescope.builtin").grep_string({
+         search = input_string
+      })
+end)
+
+vim.keymap.set('n', 'fd', function()
+   builtin.find_files({ cwd = utils.buffer_dir() })
 end)
--- init.lua
+++ init.lua
@@ -12,14 +12,13 @@ vim.opt.tabstop = 3
 vim.opt.softtabstop = 3
 vim.opt.shiftwidth = 3
 
-
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 99 
 vim.opt.isfname:append('@-@')
-
 vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = '80'
+-- vim.opt.colorcolumn = '80'
 
 vim.g.mapleader = ' '
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
