6a81ad295e7d23724cbe00a14c11d2de14087184
Nate Braukhoff <braukhoff96@gmail.com>
2023-12-27 10:26:20 -0600

    Adding pluggins for arduino

--- init.lua
+++ init.lua
@@ -8,8 +8,8 @@ vim.opt.expandtab = true
 
 vim.smartindent = true
 
-vim.opt.tabstop = 3
-vim.opt.softtabstop = 3
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 3
 
 vim.opt.termguicolors = true
