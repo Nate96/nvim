ed0181cf91c4b4bc7cdb366859d16426f66f7ce4
Nate96 <braukhoff96@gmail.com>
2024-03-23 10:29:52 -0500

    added theme

--- init.lua
+++ init.lua
@@ -60,5 +60,22 @@ vim.keymap.set('n', '<leader>v', '<C-v>')
 
 -- Toggling Spell Checker
 vim.api.nvim_set_keymap('n', '<leader>s', ':set spell!<CR>', { noremap = true, silent = true })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require("config")
+local plugins = {
+{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
