f893c294e7f913dc714915db9b54594193e9b0eb
Nate96 <braukhoff96@gmail.com>
2024-03-23 10:10:32 -0500

    cleaned directory

--- after/plugin/modes.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("n", "<leader>md", function()
-   vim.wo.relativenumber = true
-   vim.opt.scrolloff = 99
-end)
-
-vim.keymap.set("n", "<leader>mp", function()
-   vim.wo.relativenumber = false
-   vim.opt.scrolloff = 0
-end)
