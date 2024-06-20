2121806ab39a65c732089d0a92dc6fe23549ffeb
Nate Braukhoff <braukhoff96@gmail.com>
2024-06-20 15:54:59 -0500

    added condition to prevent error out

--- after/plugin/journal.lua
+++ after/plugin/journal.lua
@@ -6,31 +6,53 @@ local function jump(direction)
    local current_page = vim.api.nvim_buf_get_name(0):match(".*/(%d%d%d%d%-%d%d%-%d%d)")
    local files = vim.fn.readdir(directory)
 
-   for index, file in ipairs(files) do
-      if file:match("(%d%d%d%d%-%d%d%-%d%d)") == current_page then
-         vim.api.nvim_command("edit " .. directory .. '/' .. files[index + direction])
-         break
+   if current_page == nil then
+      print("current page name is not in the correct formate")
+   else
+      for index, file in ipairs(files) do
+         print(index)
+         if index == 1 and direction == BACKWARD then
+            print("You are at the first entry cannot jump backwards")
+            break
+         elseif index == #files and direction == FORWARD then
+            print("You are at the last entry cannot jump forward")
+            break
+         elseif file:match("(%d%d%d%d%-%d%d%-%d%d)") == current_page then
+            vim.api.nvim_command("edit " .. directory .. '/' .. files[index + direction])
+            break
+         end
       end
    end
 end
 
 local function jump_to_today()
-   local directory = vim.fn.getcwd() .. "/Journal"
-   local files = vim.fn.readdir(directory)
    local current_day = os.date("%Y-%m-%d")
-   local most_recent_page = files[#files]
+   local directory = vim.fn.getcwd() .. "/Journal"
    local current_day_of_week = os.date("%A")
 
-   if current_day == most_recent_page then
-      vim.api.nvim_command("edit " .. directory .. '/' .. files[#files])
+   local stat = vim.loop.fs_stat(directory)
+
+   if stat and stat.type == "directory" then
+      local files = vim.fn.readdir(directory)
+
+      if current_day ==  files[#files]:match("(%d%d%d%d%-%d%d%-%d%d)") then
+         vim.api.nvim_command("edit " .. directory .. '/' .. files[#files])
+      else
+         -- FORMAT: yyyy-mm-dd WEEKDAY
+         vim.api.nvim_command("edit " .. directory .. '/' .. current_day .. ' ' .. current_day_of_week .. ".md")
+      end
+
    else
-      vim.api.nvim_command("edit " .. directory .. '/' .. current_day .. ' ' .. current_day_of_week .. ".md")
+      local input = vim.fn.input("the Journal Direcory is not create, do you want to create? y/n: ")
+
+      if input == 'y' then
+         os.execute("mkdir " .. directory)
+         -- FORMAT: yyyy-mm-dd WEEKDAY
+         vim.api.nvim_command("edit " .. directory .. '/' .. os.date("%Y-%m-%d") .. ' ' .. os.date("%A") .. ".md")
+      end
    end
 end
 
 vim.keymap.set('n', '<leader>k', function () jump_to_today() end)
 vim.keymap.set('n', '<leader>j', function() jump(FORWARD) end)
 vim.keymap.set('n', '<leader>l', function() jump(BACKWARD) end)
-
--- TODO: when Journal directory is not pressent, ask user to create one. 
--- TODO: Add check for first page and last page when jumping FORWARD or BACKWARD
