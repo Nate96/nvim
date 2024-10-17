3afa743621893efda9f7682dcf8c5fbb20be44ee
Spuishy <braukhoff96@gmail.com>
2024-10-17 09:54:33 -0500

    Update modes.lua
    added Prs and Dev commands

--- after/plugin/modes.lua
+++ after/plugin/modes.lua
@@ -1,9 +1,19 @@
-vim.keymap.set("n", "<leader>md", function()
+vim.api.nvim_create_user_command("Dev", function()
    vim.wo.relativenumber = true
    vim.opt.scrolloff = 99
-end)
 
-vim.keymap.set("n", "<leader>mp", function()
+   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+   vim.api.nvim_set_hl(0, 'LineNr', { bg = "none" })
+   vim.api.nvim_set_hl(0, 'LineNr', { fg = "gray" })
+end, {})
+
+vim.api.nvim_create_user_command("Prs", function()
    vim.wo.relativenumber = false
    vim.opt.scrolloff = 0
-end)
+
+   vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
+   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+   vim.api.nvim_set_hl(0, 'LineNr', { bg = "none" })
+   vim.api.nvim_set_hl(0, 'LineNr', { fg = "gray" })
+end, {})
