8e67bc1adb106eb34bafa0966ecd80699fac1e42
Spuishy <braukhoff96@gmail.com>
2024-10-17 09:55:58 -0500

    Update modes.lua

--- after/plugin/modes.lua
+++ after/plugin/modes.lua
@@ -1,3 +1,5 @@
+
+-- Dev Mode: This mode it use when developing
 vim.api.nvim_create_user_command("Dev", function()
    vim.wo.relativenumber = true
    vim.opt.scrolloff = 99
@@ -8,6 +10,7 @@ vim.api.nvim_create_user_command("Dev", function()
    vim.api.nvim_set_hl(0, 'LineNr', { fg = "gray" })
 end, {})
 
+-- Presentation Mode: This mode makes it easier to present code with other people. 
 vim.api.nvim_create_user_command("Prs", function()
    vim.wo.relativenumber = false
    vim.opt.scrolloff = 0
