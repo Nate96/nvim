3dc65da2a1d659ea90f4fe729f0acf73e02c2908
Nate96 <braukhoff96@gmail.com>
2026-04-05 14:58:13 -0500

    config telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 require 'telescope'.setup {
    pickers = {
       find_files = {
-         find_command = { 'rg', '--files', '--hidden' },
+         find_command = { 'rg', '--files'},
       },
       live_grep = {
          additional_args = function()
@@ -42,26 +42,12 @@ require 'telescope'.setup {
 }
 
 local builtin = require('telescope.builtin')
-local utils = require('telescope.utils')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fF', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fH', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fr', builtin.resume, {})
-
-vim.keymap.set('n', '<leader>fgit', builtin.git_files, {})
-
-vim.keymap.set('n', '<leader>fs', function()
-   local input_string = vim.fn.input("Search For > ")
-   if (input_string == '') then
-      return
-   end
-   require("telescope.builtin").grep_string({
-      search = input_string
-   })
-end)
-
-vim.keymap.set('n', '<leader>fd', function()
-   builtin.find_files({ cwd = utils.buffer_dir() })
-end)
+vim.keymap.set('n', '<leader>fg', function()
+    builtin.find_files({ hidden = true, no_ignore = true, find_command = { 'rg', '--files', '--hidden' } })
+end, {})
